96a48debf4b59b7c04e9f6ef04e7cfd1e7624201
dml <15191681021@163.com>
2021-11-13 11:41:11 +0800

    add cors config

---
+++ src/main/java/com/ml/config/GlobalCorsConfig.java
@@ -0,0 +1,30 @@
+package com.ml.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @author dml
+ * @date 2021/11/13 11:29
+ */
+@Configuration
+public class GlobalCorsConfig {
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOriginPatterns("*")
+                        .allowCredentials(true)
+                        .allowedMethods("GET", "POST")
+                        .allowedHeaders("*")
+                        .exposedHeaders("Header1", "Header2");
+            }
+        };
+    }
+
+}
